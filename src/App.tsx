ecb37bdc20390ef255dac28302525ef8b05451e6
koikoi622 <104248045+koikoi622@users.noreply.github.com>
2022-05-05 15:29:26 +0900

    05051529

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import "./styles.css";
-import { UserCard } from "./components/UserCard";
-import { useAllUsers } from "./hooks/useAllUsers";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
-  const { getUsers, userProfiles, loading, error } = useAllUsers();
-  const onClickFetchUser = () => {
-    getUsers();
-  };
   return (
-    <div className="App">
-      <button onClick={onClickFetchUser}>データ取得</button>
-      <br />
-      {error ? (
-        <p style={{ color: "red" }}>データの取得に失敗しました</p>
-      ) : loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {userProfiles.map((user) => (
-            <UserCard key={user.id} user={user} />
-          ))}
-        </>
-      )}
-    </div>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
---
+++ src/components/organisms/layout/Header.tsx
@@ -0,0 +1,18 @@
+import { Flex, Heading } from "@chakra-ui/react";
+import { memo, VFC } from "react";
+
+export const Header: VFC = memo(() => {
+  return (
+    <Flex
+      as="nav"
+      bg="teal.500"
+      color="gray.50"
+      align="center"
+      justify="space-between"
+    >
+      <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+        ユーザ管理アプリ
+      </Heading>
+    </Flex>
+  );
+});
